df8c96590a93c32ca180e6bec8780ff1760f2dfa
Ashish Patel <toaashishpatel@gmail.com>
2019-12-21 01:24:30 +0530

    Console App Demo - Added console application

---
+++ src/AzureAppConfigurationLabs.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureAppConfigurationLabs.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
